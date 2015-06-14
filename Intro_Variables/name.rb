a467d41d37517b3f653e9d571819b5f290ef0372
jc123junk <jc123.junk@gmail.com>
2015-06-14 19:50:41 +0000

    Adding in the new folder with Intro_Variables

---
+++ Intro_Variables/name.rb
@@ -0,0 +1,14 @@
+#Variables Section - Exercise 1,3,4
+puts "Please enter your first name"
+first_name = gets.chomp
+puts "Please enter your last name"
+last_name = gets.chomp
+
+
+10.times {puts "The fullname you have entere is: #{first_name} #{last_name}"}
+
+#Exercise 5
+# Code snippet 1 will return 3 because x is declared outside of the do. Code snippit 2 will return error becuase x is declared within the do loop
+
+#Exercise 6
+#You're trying to call shoes without it being defined or within scope
